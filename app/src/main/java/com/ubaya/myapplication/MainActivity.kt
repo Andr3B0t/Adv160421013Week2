410405568ac82a7074d6d404045feb47e672fd1b
Andr3B0t <andreasjcool281@gmail.com>
2024-02-29 14:46:47 +0700

    Added navigations

--- app/src/main/java/com/ubaya/myapplication/MainActivity.kt
+++ app/src/main/java/com/ubaya/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ubaya.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ubaya.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
--- app/src/main/java/com/ubaya/myapplication/MainFragment.kt
+++ app/src/main/java/com/ubaya/myapplication/MainFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ubaya.myapplication.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
-
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(
+            inflater,
+            container, false
+        )
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnStart.setOnClickListener {
+            val action = MainFragmentDirections.actionGameFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 }
