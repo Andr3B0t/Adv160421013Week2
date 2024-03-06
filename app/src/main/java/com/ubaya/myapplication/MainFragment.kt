d5ee1fab18a692b4d76f4ec6cab396c7784abb37
Andr3B0t <andreasjcool281@gmail.com>
2024-03-06 17:26:37 +0700

    week 2 homework

--- app/src/main/java/com/ubaya/myapplication/MainFragment.kt
+++ app/src/main/java/com/ubaya/myapplication/MainFragment.kt
@@ -7,10 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.ubaya.myapplication.databinding.FragmentMainBinding
+import kotlin.random.Random
 
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    var score = 0
+    var answer =0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,13 +26,30 @@ class MainFragment : Fragment() {
         return binding.root
 
     }
-
+    fun updateQuestion() {
+        var number1 = Random.nextInt(1,69)
+        var number2 = Random.nextInt(1,69)
+        answer = number1 + number2
+        var question = number1.toString()+" + "+number2.toString()
+        binding.txtQuestion.setText(question)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        score = 0
+        updateQuestion()
         binding.btnStart.setOnClickListener {
             val playerName = binding.txtName.text.toString()
             val action = MainFragmentDirections.actionGameFragment(playerName)
             Navigation.findNavController(it).navigate(action)
+        }
+        binding.btnAnswer.setOnClickListener {
+            if (binding.txtAnswer.text.toString()==answer.toString()){
+                score++
+                updateQuestion()
+            } else {
+                val action = MainFragmentDirections.actionResultFragment(score.toString())
+                Navigation.findNavController(it).navigate(action)
+            }
         }
 
     }
